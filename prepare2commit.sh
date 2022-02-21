956b206e937173d394558cb0a6fed5d95fa34030
Alisson Chiquitto <chiquitto@gmail.com>
2022-02-21 17:38:11 -0400

    Input files

--- prepare2commit.sh
+++ prepare2commit.sh
@@ -6,7 +6,11 @@
 # tar -cvf - input/* | xz -v > input.tar.xz
 
 tar -cvf - input/C174-PHASED.fa | xz -v > input/C174-PHASED.fa.xz
-split -b 90m - input/C174-PHASED.fa.xz
+# split into smaller parts for github
+split --verbose -b90M input/C174-PHASED.fa.xz input/C174-PHASED.fa.xz.
+rm -v input/C174-PHASED.fa.xz
+# to reassemble
+# cat input/C174-PHASED.fa.xz.?? > input/C174-PHASED.fa.xz
 
 tar -cvf - input/ResultadoFinal_C174-PHASED.gff | xz -v > input/ResultadoFinal_C174-PHASED.gff.fa.xz
 tar -cvf - input/annot-genes/* | xz -v > input/annot-genes.fa.xz
